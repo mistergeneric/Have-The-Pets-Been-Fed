5d80a0b839c3fc4bb58eb2b2861cb4e00f4b295c
mistergeneric <andrewmcneill1992@gmail.com>
2020-07-12 17:37:12 +0100

    added readme file

--- havePetsBeenFed/lambda_function.py
+++ havePetsBeenFed/lambda_function.py
@@ -1,8 +1,37 @@
 import json
 
+
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
+def on_start():
+    print("Session Started.")
+
+
+def on_launch(event):
+    onlunch_MSG = "Hi, and welcome to have the pets been fed"
+    reprompt_MSG = "Have the pets been fed?"
+    card_TEXT = "Tell us whether the pets have been fed."
+    card_TITLE = "Fed a pet."
+    return output_json_builder_with_reprompt_and_card(onlunch_MSG, card_TEXT, card_TITLE, reprompt_MSG, False)
+
+
+def on_end():
+    print("Session Ended.")
+
+
+def intent_scheme(event):
+    # change this intent name
+    intent_name = event['request']['intent']['name']if intent_name == "feedThePet":
+        return feed_the_pet(event)
+    elif intent_name in ["AMAZON.NoIntent", "AMAZON.StopIntent", "AMAZON.CancelIntent"]:
+        return stop_the_skill(event)
+    elif intent_name == "AMAZON.HelpIntent":
+        return assistance(event)
+    elif intent_name == "AMAZON.FallbackIntent":
+        return fallback_call(event)
---
+++ havePetsBeenFed/readme.md
@@ -0,0 +1,8 @@
+# Have the Pets Been Fed
+
+This is the lamdba for the Alexa skill I am making which will track whether or not someone in the house has fed the cat, or not.
+
+## Usage
+
+This is intended to be a fully fledged Alexa Skill available soon!
+
